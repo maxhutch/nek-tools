5d2687dcd6bb58b9c6bbce40a097b5d9ed1ed12c
Max Hutchinson <maxhutch@gmail.com>
2013-12-12 21:22:44 -0600

    Added argparse for cmdline args and more intelligent paths.

--- genrun/genrun.py
+++ genrun/genrun.py
@@ -1,34 +1,44 @@
 #!/usr/bin/python3
 from sys import argv
 from os import system
+from os import path
+import argparse
 import json
 
-with open("./default.json", "r") as f:
+parser = argparse.ArgumentParser(description="Generate NEK inputs")
+parser.add_argument('name', help="Name to assign to generated system")
+parser.add_argument('-d', '--dict', dest='config', help="Dictionary of parametesr (JSON)")
+parser.add_argument('-u', '--usr', dest='usr', help="*.usr file to use for build")
+
+args = parser.parse_args()
+
+mypath = (path.realpath(__file__))[:-9]
+
+with open(path.join(mypath, "default.json"), "r") as f:
   default_config = json.load(f)
 
 loaded_config = {}
-if len(argv) > 1:
-  with open(argv[1], 'r') as f:
+if args.config != None:
+  with open(args.config, 'r') as f:
     loaded_config = json.load(f)
-    print("Loaded config")
 
 config = dict(list(default_config.items()) + list(loaded_config.items()))
 
-with open("./template.SIZE", "r") as f:
+with open(path.join(mypath, "template.SIZE"), "r") as f:
   size_template = f.read()
 size = size_template.format(**config)
 
 with open("./SIZE", "w") as f:
   f.write(size)
 
-with open("./template.rea", "r") as f:
+with open(path.join(mypath, "template.rea"), "r") as f:
   rea_template = f.read()
 rea = rea_template.format(**config)
 
 with open("./tmp.rea", "w") as f:
   f.write(rea)
 
-with open("./template.box", "r") as f:
+with open(path.join(mypath, "template.box"), "r") as f:
   box_template = f.read()
 box = box_template.format(**config)
 
@@ -39,5 +49,10 @@ with open("./test.json", "w") as f:
   json.dump(config, f, indent=2)
 
 system("echo 'tmp.box' | genbox")
-system("makenek box")
+system("mv box.rea "+args.name+".rea")
+system("mv box.re2 "+args.name+".re2")
+if args.usr != None:
+  system("cp "+ args.usr + " "+args.name+".usr")
+system("makenek clean")
+system("makenek "+args.name)
 
