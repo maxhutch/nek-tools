afe6a6b531df7528ed847e663ba38a502733cb62
Max Hutchinson <maxhutch@gmail.com>
2013-12-12 22:59:05 -0600

    Added configuration processing, nproc cmdline arg, and genmap call.
    The script can apply relations like:
    number of elements = (number of elements in x) * (" " in y) * (" " in z)
    This is done by loading the configuration dict into the current
    variable scope, manipulating by parameter name, and then dumping
    the current variable scope back into the configuration dict.
    
    The number of processors can be specified on the command line, which
    overrides the value in the configuration.
    
    Added a call to genmap.

--- genrun/genrun.py
+++ genrun/genrun.py
@@ -5,13 +5,15 @@ from os import path
 import argparse
 import json
 
+''' Loading stuff '''
+
 parser = argparse.ArgumentParser(description="Generate NEK inputs")
 parser.add_argument('name', help="Name to assign to generated system")
 parser.add_argument('-d', '--dict', dest='config', help="Dictionary of parametesr (JSON)")
 parser.add_argument('-u', '--usr', dest='usr', help="*.usr file to use for build")
+parser.add_argument('-n', '--nproc', dest='np', type=int, default=-1, help="Number of processes to target")
 
 args = parser.parse_args()
-
 mypath = (path.realpath(__file__))[:-9]
 
 with open(path.join(mypath, "default.json"), "r") as f:
@@ -23,6 +25,24 @@ if args.config != None:
     loaded_config = json.load(f)
 
 config = dict(list(default_config.items()) + list(loaded_config.items()))
+
+with open("./test.json", "w") as f:
+  json.dump(config, f, indent=2)
+
+''' Computing stuff '''
+
+# loads the configuration into current variable scope
+locals().update(config)
+
+# Manipulate the configuration here
+elements_total = shape_mesh[0] * shape_mesh[1] * shape_mesh[2]
+if args.np > 0: 
+  procs = args.np
+
+# writes the current variable scope to the configuration
+config = locals()
+
+''' Writing stuff '''
 
 with open(path.join(mypath, "template.SIZE"), "r") as f:
   size_template = f.read()
@@ -45,12 +65,10 @@ box = box_template.format(**config)
 with open("./tmp.box", "w") as f:
   f.write(box)
 
-with open("./test.json", "w") as f:
-  json.dump(config, f, indent=2)
-
 system("echo 'tmp.box' | genbox")
 system("mv box.rea "+args.name+".rea")
 system("mv box.re2 "+args.name+".re2")
+system("echo '"+args.name+"' | genmap")
 if args.usr != None:
   system("cp "+ args.usr + " "+args.name+".usr")
 system("makenek clean")
